2baf951dae642a80ece231aab6ffc775671a75f5
Manolo Bevia <manolo.bevia@cornell.edu>
2016-02-09 13:12:34 -0500

    Worked on highlights carousel

--- js/utils.js
+++ js/utils.js
@@ -217,20 +217,20 @@ $( document ).ready(function() {
           infinite: false,
           speed: 300,
           slidesToShow: 3,
-          slidesToScroll: 1,
-          
+          slidesToScroll: 3,
+
           responsive: [
             {
               breakpoint: 1024,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true,
+                infinite: false,
                 dots: true
               }
             },
             {
-              breakpoint: 600,
+              breakpoint: 640,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
